a8d0685dde75e5a8b4f61ba9898b76632edd554c
marko3-3 <marko3@poczta.fm>
2020-06-04 17:10:30 +0200

    modyfikacja pliku main , dodanie 123  odpisanie cyfry 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Uczę");
         System.out.println("się");
-        System.out.println("programowania1234");
+        System.out.println("programowania123");
 
     }
 }
